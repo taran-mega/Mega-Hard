cc5d4b56eb6b806d91aeba548b071a9df4612d25
taran-mega <taranjit260526@gmail.com>
2026-05-26 13:42:49 +0530

    Update game.js

--- Blaster/game.js
+++ Blaster/game.js
@@ -3,9 +3,17 @@ const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
 const msg_box = document.getElementsByClassName("death_msg_popup");
 
+const dpr = window.devicePixelRatio || 1;
+
+canvas.width = window.innerWidth * dpr;
+canvas.height = window.innerHeight * dpr;
+
+canvas.style.width = window.innerWidth + "px";
+canvas.style.height = window.innerHeight + "px";
+
+ctx.scale(dpr, dpr);
+
 // Screen Variables
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
 const height = canvas.height;
 const width = canvas.width;
 
@@ -885,4 +893,4 @@ function gameloop() {
 }
 
 // Call Function
-gameloop();
+window.onload() => 
