f5881a47454b30e99ece663b49a9af4b198af1c3
taran-mega <taranjit260526@gmail.com>
2026-06-07 10:40:12 +0530

    Update firebase.js

--- Blaster/firebase.js
+++ Blaster/firebase.js
@@ -1,1 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyALa11wSSCeDgOZVhhqi43MlO6XRNB2oJU",
+  authDomain: "blaster-1488a.firebaseapp.com",
+  projectId: "blaster-1488a",
+  storageBucket: "blaster-1488a.firebasestorage.app",
+  messagingSenderId: "417513539683",
+  appId: "1:417513539683:web:62d5895cba2359ff55d056",
+  measurementId: "G-7VZY8E3L5C"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
